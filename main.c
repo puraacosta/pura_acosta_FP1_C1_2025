8820953ba9a875bba9c7752d70243250363e7810
puraacosta <puraacosta15@gmail.com>
2025-02-07 11:09:35 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* promedio curso.
+el programa, al recibir como dato el promedio de un alumno en un curso
+ universitario, escribe aprobado si su promedio es
+mayor o igual a 6
+
+pro: varibale de tipo real. */
+
+
+void main (void)
+ {
+     float pro;
+     printf(*60-100*);
+     scanf ("%f", "&pro")
+     if (0 >=6)
+    printf
+
+ }
