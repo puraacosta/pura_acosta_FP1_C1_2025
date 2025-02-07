d33f2d4bd9d927a071eca00a995e3720753a429d
puraacosta <puraacosta15@gmail.com>
2025-02-07 11:15:13 -0400

    Add files via upload
    ejemplo 10

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ printf(“Ingrese los valores de R, T y Q: “);
 scanf(“%d %d %d”, &R, &T, &Q);
 RES = pow(R, 4) – pow(T, 3) + 4 * pow(Q, 2);
 
+
 if (RES < 820)
 printf(“\nR = %d\tT = %d\t Q = %d”, R, T, Q);
 
