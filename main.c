ac57525747c5d5db5cc35300330982e5aa92b895
puraacosta <puraacosta15@gmail.com>
2025-03-14 13:44:56 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
-#include <math.h>
-/* Igualdad de expresiones.
-El programa, al recibir como datos T, P y N, comprueba la igualdad de
-una expresi�n determinada.
-T, P y N: variables de tipo entero. */
-void main(void)
-{#include <stdio.h>
-#include <math.h>
-/* Igualdad de expresiones.
-El programa, al recibir como datos T, P y N, comprueba la igualdad de
-una expresi�n determinada.
-T, P y N: variables de tipo entero. */
+/* Diagonal principal.
+El programa, al recibir como dato una matriz de tipo entero, escribe la
+diagonal principal. */
+const int TAM = 10;
+void Lectura(int [][TAM], int); /* Prototipo de funciones. */
+void Imprime(int [][TAM], int);
+/* Observa que siempre es necesario declarar el número de columnas. Si no lo
+➥haces, el compilador marcará un error de sintaxis. */
 void main(void)
 {
+int MAT[TAM][TAM];
+Lectura(MAT, TAM);
+Imprime(MAT, TAM);
+}
+void Lectura(int A[][TAM], int F)
+/* La función Lectura se utiliza para leer un arreglo bidimensional. Observa
+➥que sólo se debe pasar como parámetro el número de filas ya que la matriz
+➥es cuadrada. */
+{
+int I, J;
+for (I=0; I<F; I++)
+for (J=0; J<F; J++)
+{
+printf(”Ingrese el elemento %d %d: ”, I+1, J+1);
+scanf(”%d”, &A[I][J]);
+}
+}
+void Imprime(int A[][TAM], int F)
+/* La función Imprime se utiliza para escribir un arreglo bidimensional
+➥cuadrado de F filas y columnas. */
+{
+int I, J;
+for (I=0; I<F; I++)
+for (J=0; J<TAM; J++)
+if (I == J)
+printf(”\nDiagonal %d %d: %d ”, I, J, A[I][J]);
+}
