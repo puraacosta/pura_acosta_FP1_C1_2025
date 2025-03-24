bb635cec3e9f9efdee56dfecba3eca6d8caab0df
puraacosta <puraacosta15@gmail.com>
2025-03-24 18:51:53 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,22 @@
 #include <stdio.h>
-/* Diagonal principal.
-El programa, al recibir como dato una matriz de tipo entero, escribe la
-diagonal principal. */
-const int TAM = 10;
-void Lectura(int [][TAM], int); /* Prototipo de funciones. */
-void Imprime(int [][TAM], int);
-/* Observa que siempre es necesario declarar el número de columnas. Si no lo
-➥haces, el compilador marcará un error de sintaxis. */
+#include <math.h>
+/* Expresión.
+El programa, al recibir como datos tres valores enteros, establece si los
+➥mismos satisfacen una expresión determinada.
+R, T y Q: variables de tipo entero.
+RES: variable de tipo real. */
 void main(void)
 {
-int MAT[TAM][TAM];
-Lectura(MAT, TAM);
-Imprime(MAT, TAM);
-}
-void Lectura(int A[][TAM], int F)
-/* La función Lectura se utiliza para leer un arreglo bidimensional. Observa
-➥que sólo se debe pasar como parámetro el número de filas ya que la matriz
-➥es cuadrada. */
-{
-int I, J;
-for (I=0; I<F; I++)
-for (J=0; J<F; J++)
-{
-printf(”Ingrese el elemento %d %d: ”, I+1, J+1);
-scanf(”%d”, &A[I][J]);
-}
-}
-void Imprime(int A[][TAM], int F)
-/* La función Imprime se utiliza para escribir un arreglo bidimensional
-➥cuadrado de F filas y columnas. */
-{
-int I, J;
-for (I=0; I<F; I++)
-for (J=0; J<TAM; J++)
-if (I == J)
-printf(”\nDiagonal %d %d: %d ”, I, J, A[I][J]);
+float RES;
+
+int R, T, Q;
+printf(“Ingrese los valores de R, T y Q: “);
+
+scanf(“%d %d %d”, &R, &T, &Q);
+RES = pow(R, 4) – pow(T, 3) + 4 * pow(Q, 2);
+
+
+if (RES < 820)
+printf(“\nR = %d\tT = %d\t Q = %d”, R, T, Q);
+
 }
